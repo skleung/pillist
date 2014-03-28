6e8b99f0e7fb9df92d10fc6c99546941990cbf88
Sherman Leung <skleung@stanford.edu>
2014-03-27 23:19:22 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+	def new
+		@user
+	end
+	def third
+		@user
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pillist::Application.config.secret_key_base = '5db2e6fa3be1ed098c053b61438e0219d1a2f0a0a237fc4d1d745d9b5a2a1658bd822a52b9567511271c86cc207c7bd521e07126e9fc628e0691c0f5300e11fb'
