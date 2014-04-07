50bf5de923ad453a55401b2452d7a8089e410ffc
Sherman Leung <skleung@stanford.edu>
2014-04-07 14:41:04 -0700

    added down arrow

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,27 @@
+$(document).ready(function(){
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > $(window).height()) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 500);
+			return false;
+		});
+	});
+
+});
 $(document).ready(function($) { 
 	$('.landing-background') .css({'min-height': $(window).height() +'px'});
 	$('#home').css({'min-height': $(window).height() +'px'});
